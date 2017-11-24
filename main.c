bec0f1525200ede0fbaecbc9627e15078ed8c2aa
gborowikwspol <grzesiu.borowik@gmail>
2017-11-24 08:58:23 +0100

    prawie zakonczone

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 void test_oblicz_rownanie_kwadratowe();
 
@@ -6,10 +7,25 @@ void test_oblicz_rodzaj_rw_kw();
 
 void test_oblicz_delta();
 
+void test_oblicz_pierwiastek1();
+
+void test_oblicz_pierwiastek2();
+
+double oblicz_delta(double a, double b, double c);
+
+double oblicz_pierwiastek1(double b, double delta, double a) {
+    return ((-b - sqrt(delta)) / (2 * a));
+}
+
+double oblicz_pierwiastek2(double b, double delta, double a) {
+    return ((-b + sqrt(delta)) / (2 * a));
+}
+
 void oblicz_rownanie_kw(double a, double b, double c, double *x1, double *x2, int *rodzaj) {
-    *x1 = 0.5;
-    *x2 = 2.0;
-    *rodzaj = 2;
+    *rodzaj = oblicz_rodzaj_rw_kw(a, b, c);
+
+    *x1 = oblicz_pierwiastek1(b, oblicz_delta(a, b, c), a);
+    *x2 = oblicz_pierwiastek2(b, oblicz_delta(a, b, c), a);
 }
 
 double oblicz_delta(double a, double b, double c) {
@@ -35,6 +51,36 @@ int main() {
     test_oblicz_rownanie_kwadratowe();
     test_oblicz_rodzaj_rw_kw();
     test_oblicz_delta();
+    test_oblicz_pierwiastek1();
+    test_oblicz_pierwiastek2();
+}
+
+void test_oblicz_pierwiastek2() {
+    double pierwiastek;
+
+    pierwiastek = oblicz_pierwiastek2(2.0, 4.0, 6.0);
+    if (pierwiastek != (0.0)) {
+        printf("Blad funkcji oblicz_pierwiastek1, pierwiastek = %lf\n", pierwiastek);
+    }
+
+    pierwiastek = oblicz_pierwiastek2(5.0, 16.0, 3.0);
+    if (pierwiastek != (-1.0 / 6.0)) {
+        printf("Blad funkcji oblicz_pierwiastek1, pierwiastek = %lf\n", pierwiastek);
+    }
+}
+
+void test_oblicz_pierwiastek1() {
+    double pierwiastek;
+
+    pierwiastek = oblicz_pierwiastek1(2.0, 4.0, 6.0);
+    if (pierwiastek != (-4.0 / 12.0)) {
+        printf("Blad funkcji oblicz_pierwiastek1, pierwiastek = %lf\n", pierwiastek);
+    }
+
+    pierwiastek = oblicz_pierwiastek1(5.0, 16.0, 3.0);
+    if (pierwiastek != (-9.0 / 6.0)) {
+        printf("Blad funkcji oblicz_pierwiastek1, pierwiastek = %lf\n", pierwiastek);
+    }
 }
 
 void test_oblicz_delta() {
@@ -42,6 +88,11 @@ void test_oblicz_delta() {
 
     delta = oblicz_delta(2.0, -5.0, 2.0);
     if (delta != 9.0) {
+        printf("Blad funkcji oblicz_delta, delta = %lf", delta);
+    }
+
+    delta = oblicz_delta(6.0, -8.0, -2.6);
+    if (delta != 126.4) {
         printf("Blad funkcji oblicz_delta, delta = %lf", delta);
     }
 
